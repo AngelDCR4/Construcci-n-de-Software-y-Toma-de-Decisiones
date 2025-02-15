82147a2d39e08f62a27c787929ad4b6691b53c1e
Angel D CR <Angel>
2025-02-15 17:21:32 -0600

    Segundo ejercicio Laboratorio 4

--- lab4/js/lab4.js
+++ lab4/js/lab4.js
@@ -1,6 +1,7 @@
-
+//Prueba de java funcionando
 console.log("XD");
 
+/* EJERCICIO 1*/
 function mostrartabla() {
     //Solicitar número
     let numero = parseInt(prompt("Por favor ingrese un número para la tabla:"));
@@ -21,5 +22,38 @@ function mostrartabla() {
     document.write("</table>")
 }
 
-mostrartabla();
+//mostrartabla();
 
+/*EJERCICIO 2 */
+
+function suma_aleatoria() {
+          //Math.floor redondea un numero
+                     //Math.random genera número aleatorio
+    let a = Math.floor(Math.random() * 10) + 1; //Se coloca el *10 para generar números de [ 0,10 ) -> 0, 1, 2... hasta 9.999
+    let b = Math.floor(Math.random() * 10) + 1; //Math.floor redondea el número random y se suma 1 para no dar el número 0 [ 1,10 ]
+
+    //Toma de tiempo
+    let tiempo = new Date().getTime();
+
+    //Solicitud de respuesta
+    let res = parseInt(prompt("¡¡RAPIDO!! ¿Cuanto es " + a + " + " + b + "?"));
+    
+    //Fin de toma de timepo
+    let tiempofinal = new Date().getTime();
+    
+    //Calcular tiempo total
+    let tiempototal = (tiempofinal - tiempo) / 1000; //se divide para tener los microsegundos
+
+    //Validar respuesta
+    if (res === (a + b)){
+        document.write(`¡ES CORRECTO! | La suma de ${a} + ${b} es <b>${a + b}<b><br>`);
+    } else {
+        //El ${} se usa para crear una sola cadena de texto en lugar de concatenar con + +
+        document.write(`Es incorrecto :c | La respuesta es <b>${a + b}<b><br>`);
+    }
+
+    //Mostrar tiempo
+    document.write(`Tiempo tomado: ${tiempototal} segundo`);
+}
+
+suma_aleatoria();
