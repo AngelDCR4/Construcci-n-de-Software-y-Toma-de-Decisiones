973bbc16a23685d7c64b6b53b191af0daac14af7
Angel D CR <Angel>
2025-02-25 15:48:50 -0600

    Jacob: Obtenicion de datos en input

--- lab10/app.js
+++ lab10/app.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const http = require('http');
+const personajes = [];
+
+
 
 const main = `<!DOCTYPE html>
 <html>
@@ -10,7 +13,39 @@ const main = `<!DOCTYPE html>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
   </head>
   <body>
-    <h1>Hola Mundo a todods  </h1>
+    <h1><b>Hola Mundo a todo</b></h1>
+
+    <br>
+    <br>
+
+    <div class="columns">
+      <div class="column"><figure class="image is-128x128">
+      <img class="is-rounded" src="C:\\Users\\angel\\Documents\\C++\\.vscode\\Construcci-n-de-Software-y-Toma-de-Decisiones\\lab10\\jacob.jpg'">
+        </figure>
+</div>
+      <div class="column"> 
+      <figure class="image is-128x128">
+        <img class="is-rounded" src="rolon.jpg" alt="Imagen de rolon">
+      </figure></div>
+    </div>
+
+    <form action="/extra" method="POST">
+            <div class="field">
+              <p class="control has-icons-left">
+                <input class="input" type="password" name="password" placeholder="Password">
+                <span class="icon is-small is-left">
+                  <i class="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+            <br>
+            <br>
+            <div class="field">
+              <p class="control">
+                <button class="button is-success" type="submit">Login</button> 
+              </p>
+            </div>
+          </form>
   </body>
 </html>
 `;
@@ -37,30 +72,15 @@ const html_main = `
 
   <section class="section">
     <div class="container">
-        <div class="field">
-            <p class="control has-icons-left has-icons-right">
-              <input class="input" type="email" name="email" placeholder="Email">
-              <span class="icon is-small is-left">
-                <i class="fas fa-envelope"></i>
-              </span>
-              <span class="icon is-small is-right">
-                <i class="fas fa-check"></i>
-              </span>
-            </p>
-          </div>
+        <h1 class="title is-1">Inserta un nombre</h1>
           
-          <form action="/" method="POST">
+          <form action="/new" method="POST">
             <div class="field">
               <p class="control has-icons-left">
-                <input class="input" type="password" name="password" placeholder="Password">
+                <input class="input" type="text" name="nombre" placeholder="Jacob">
                 <span class="icon is-small is-left">
                   <i class="fas fa-lock"></i>
                 </span>
-              </p>
-            </div>
-            <div class="field">
-              <p class="control">
-                <button class="button is-success" type="submit">Login</button> 
               </p>
             </div>
           </form>
@@ -70,22 +90,73 @@ const html_main = `
 </html>
 `;
 
+
+const extra = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LAB10</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+  </head>
+  <body>
+    <h1>Hola tonotos </h1>
+  </body>
+</html>
+`;
+
+const html_card=`<div style="margin-top:30px;" class="card">
+  <div class="card-content">
+    <div class="content">
+      
+    `
+
+const html_footer =`<br>
+        </div>
+      </div>
+  </div>`
+
+
+
+
+
 const server = http.createServer((request, response) => {
   if (request.method === "GET") {
     response.writeHead(200, { "Content-Type": "text/html" }); 
     response.write(html_main);
     response.end();
   } else if (request.method === "POST") {
-    let body = "";
+    const datos = [];
+    request.on('data',(dato)=>{
+      console.log(dato);
+      datos.push(dato);
+    })
 
-    request.on("data", chunk => {
-      body += chunk.toString();
-    });
 
     request.on("end", () => {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(main);
-      response.end();
+      if(request.url=="/new"){
+        const datos_completos = Buffer.concat(datos).toString();
+        console.log(datos_completos);
+        personajes.push(datos_completos.split('=')[1]);
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(main);
+        response.write('<div class="columns">');
+        for(const personaje of personajes){
+          response.write(html_card);
+          response.write(personaje);
+          response.write(html_footer);
+          response.write('</div');
+        }
+
+
+        response.end();
+      }else if(request.url=="/extra"){
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(extra)
+        response.end();
+      }
+      
     });
   }
 });
