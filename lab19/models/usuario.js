59b1700b7a5e3a276df7d0a16672f16f2d88b089
Angel D CR <Angel>
2025-03-16 21:22:42 -0600

    Metodo para obtener rol y permisos de un usuario en Modelo usuario

--- lab19/models/usuario.js
+++ lab19/models/usuario.js
@@ -21,4 +21,18 @@ module.exports = class Usuario {
     static findByUsername(username) {
         return db.execute('SELECT * FROM usuarios WHERE username = ?', [username]);
     }
+
+//Agregar método para obtener los roles y permisos de cierto usuario
+    //Metodo estatico y asincrono para la consulta
+    static async getRolesAndPermissions(userId) {
+        return db.execute( //Definición de consulta
+            `SELECT r.nombre as rol, p.nombre as permiso 
+             FROM usuario_roles ur
+             JOIN roles r ON ur.rol_id = r.id
+             JOIN rol_permisos rp ON r.id = rp.rol_id
+             JOIN permisos p ON rp.permiso_id = p.id
+             WHERE ur.usuario_id = ?`, //Filtra por el ID del usuario
+             [userId] //ID que usa la consulta
+        )
+    }
 };
