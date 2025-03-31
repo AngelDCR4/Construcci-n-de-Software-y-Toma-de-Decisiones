1cf9477715e71deb948f1c2c1d3fed39480e2e8b
Angel D CR <Angel>
2025-03-31 10:11:51 -0600

    Creacaión de rama y agregar bibliotecas npm

---
+++ lab26/models/mensaje.js
@@ -0,0 +1,21 @@
+const db = require('../util/database'); //Importar la conexión a la BD
+
+module.exports = class Mensaje {
+    constructor(nombre, contenido) {
+        this.nombre = nombre;
+        this.contenido = contenido;
+    }
+
+    //Guardar mensaje en base de datos
+    save() {
+        //Inserta los datos en la BD y no acepta inyecciones
+        return db.execute('CALL insertar_mensaje(?, ?)',
+                         [this.nombre, this.contenido]);
+    }
+
+    
+    //Obtener todos los mensajes de la BD
+    static fetchAll() {
+        return db.execute('CALL obtener_mensajes()');
+    }
+};
